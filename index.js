a6727c41e392b776f4ec4e3fc011e0165ccc7b52
AsmaZag <zagmouzi.asma@ine.inpt.ma>
2023-05-29 18:25:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+
+// Configuration de la session
+app.use(session({
+  secret: 'votre-secret-de-session',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Middleware pour parser les données du formulaire
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Lecture du fichier JSON contenant les utilisateurs et les mots de passe
+const usersData = fs.readFileSync('users.json');
+const users = JSON.parse(usersData);
+
+// Middleware pour vérifier l'authentification de l'utilisateur
+const isAuthenticated = (req, res, next) => {
+  if (req.session.isAuthenticated) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
+// Page d'accueil (accessible uniquement aux utilisateurs authentifiés)
+app.get('/', isAuthenticated, (req, res) => {
+    const filePath = '/Users/Asma ZAGMOUZI/Etudes/INPT ASEDS INE2/sec-project/dashboard.html';
+    res.sendFile(filePath);
+});
+
+// Page de connexion
+app.get('/login', (req, res) => {
+    const filePath = '/Users/Asma ZAGMOUZI/Etudes/INPT ASEDS INE2/sec-project/login.html';
+    res.sendFile(filePath);
+});
+
+// Gestion de la soumission du formulaire de connexion
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Recherche de l'utilisateur dans la liste
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (user) {
+    req.session.isAuthenticated = true;
+    res.redirect('/');
+  } else {
+    //res.send('Nom d\'utilisateur ou mot de passe incorrect');
+    res.send(`
+      <script>
+        window.onload = function() {
+          document.getElementById('alert-msg').classList.remove('hidden');
+        };
+      </script>
+      <a href="/">Retour à la page de connexion</a>
+    `);
+  }
+});
+
+// Déconnexion de l'utilisateur
+app.get('/logout', (req, res) => {
+  req.session.isAuthenticated = false;
+  res.redirect('/login');
+});
+app.get('/:nomPage', (req, res) => {
+  const nomPage = req.params.nomPage;
+  if (req.session.isAuthenticated){
+    res.sendFile(`${nomPage}`, { root: __dirname + '/' }, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(404).send('Page not found');
+      }
+    });
+  
+  
+  }
+});
+// Port d'écoute du serveur
+const port = 3000;
+
+// Démarrage du serveur
+app.listen(port, () => {
+  console.log(`Serveur en écoute sur le port ${port}`);
+});
